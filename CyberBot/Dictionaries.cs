8248e89af79e20471db5835e8e8139eed5f04424
Adriaan ST10263443 <st10263443@vcconnect.edu.za>
2025-05-26 01:23:16 +0200

    Fixed error A fatal error occured where all the code was lost however I was able to get it back. Previous comments on the code can be found in master branch. In summary all the criteria of the program has been addressed finishing touches still have to be added.

---
+++ CyberBot/Dictionaries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberBot
+{
+    internal class Dictionaries
+    {
+        //Dictionary to hold various cybersecurity topics and their corresponding tips retrieval methods.
+        public static Dictionary<string, Func<string>> InterestedIn = new Dictionary<string, Func<string>>
+        {
+            { "phishing", () => Lists.PhishingTips() },
+            { "password", () => Lists.PasswordTips() },
+            {"online safety", () => Lists.OnlineSafetyTips() },
+            { "scam", () => Lists.ScamTips() },
+            {"privacy", () => Lists.PrivacyTips() },
+        };
+    }
+}
